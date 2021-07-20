6627c953353c16ebc16c058ebeb8156b2d851ba7
otomohunter <58800351+otomohunter@users.noreply.github.com>
2021-07-20 12:19:00 +0900

    1.0.2

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
+
+const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlg, setFaceShowFlg] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickSwitchShowFlg = () => {
+    setFaceShowFlg(!faceShowFlg);
+  };
+
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です！</ColorfulMessage>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlg}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlg && <p>('ω')ノ</p>}
+    </>
+  );
+};
+
+export default App;
